a28b34385820659af1b4ce3568e02b93e357ba88
newsocialsandra <sandra.e.b.svensson@gmail.com>
2015-12-25 15:22:23 +0100

    First commit

---
+++ simplescraper.py
@@ -0,0 +1,17 @@
+import urllib
+from BeautifulSoup import *
+
+url = raw_input("Enter URL:")
+html = urllib.urlopen(url).read()
+soup = BeautifulSoup(html)
+
+tags = soup('span')
+
+count = 0
+summ = 0
+for t in tags:
+	summ += int(t.contents[0])
+	count += 1
+
+print "Count:", count
+print "Sum:", summ
